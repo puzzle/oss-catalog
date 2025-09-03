c91b465f7e7e283c3d6a2906bc6f46385707f8e6
Claudia Asti <asti@puzzle.ch>
2025-09-03 17:40:03 +0200

    feat: implement language switcher

--- client/src/i18n/utils.ts
+++ client/src/i18n/utils.ts
@@ -1,4 +1,5 @@
-import { ui, defaultLang } from './ui';
+import { ui, defaultLang, languages } from './ui';
+import { getRelativeLocaleUrl } from "astro:i18n";
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
@@ -10,4 +11,13 @@ export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
   }
+}
+
+export function getLocalePaths(url: URL) {
+  return Object.keys(languages).map((lang) => {
+    return {
+      lang: lang,
+      path: getRelativeLocaleUrl(lang, url.pathname.replace(/^\/[a-zA-Z-]+/, ''))
+    };
+  });
 }
