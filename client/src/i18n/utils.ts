53d2e348ec4ffc1d534125f7851848ef0b922551
Claudia Asti <asti@puzzle.ch>
2025-09-03 18:05:05 +0200

    refactor: optimize i18n utils

--- client/src/i18n/utils.ts
+++ client/src/i18n/utils.ts
@@ -1,15 +1,9 @@
 import { ui, defaultLang, languages } from './ui';
 import { getRelativeLocaleUrl } from "astro:i18n";
 
-export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
-  if (lang in ui) return lang as keyof typeof ui;
-  return defaultLang;
-}
-
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
+    return ui[lang][key];
   }
 }
 
