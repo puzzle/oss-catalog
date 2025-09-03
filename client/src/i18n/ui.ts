6a05424d7e0db75a16bf2bd78afa320180059baf
Claudia Asti <asti@puzzle.ch>
2025-09-03 14:41:12 +0200

    feat: add english translation

---
+++ client/src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+  en: "English",
+  de: "Deutsch",
+  fr: "Français",
+  it: "Italiano",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "header.logo.title": "Federal Chancellery FCh",
+    "header.title": "Open Source Software Catalogue",
+    "index.title": "OSS Catalogue",
+    "index.skip": "Skip to main content",
+    "software.title": "Detailed Information",
+    "software.back": "Back to overview",
+    "software.more": "Read more",
+    "footer.legal": "Legal",
+    "footer.privacy": "Privacy Policy",
+  },
+} as const;
